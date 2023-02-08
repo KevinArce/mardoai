d33629d334261d17bd88c7ccc7943a18de001b8f
KevinArce <iav.kevinarce@ufg.edu.sv>
2023-02-08 08:55:47 -0600

    minor updated openai config, also added analitics

--- client/script.js
+++ client/script.js
@@ -1,5 +1,9 @@
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
+import { inject } from '@vercel/analytics';
+
+inject();
+
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
